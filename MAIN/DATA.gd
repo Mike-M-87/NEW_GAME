3bdcf2724760cad8e1316f30e6640c73129f0934
Mike-M-87 <micminn87@gmail.com>
2020-07-03 15:29:01 +0300

    added DATA autoload

--- MAIN/DATA.gd
+++ MAIN/DATA.gd
@@ -1,8 +1,5 @@
 extends Node
 
-#var config = ConfigFile.new()
-#signal bullets_ready()
-
 var def_bullet_data = {
 	"gun1_tot_bullets":300,
 	"gun1_bullets":50,
@@ -18,28 +15,7 @@ var def_gun_data = {
 	"weapon_pos":Vector2(50,2),
 }
 var gun_data = {}
-#func _physics_process(delta):
-	#DATA.gun_data.gun1_tot_bullets = clamp(DATA.gun_data.gun1_tot_bullets,0,default_gun_data.gun1_tot_bullets)
-	#DATA.gun_data.gun2_tot_bullets = clamp(DATA.gun_data.gun2_tot_bullets,0,default_gun_data.gun2_tot_bullets)
-#	pass
-#func start():
-#	var file = File.new()
-#	if !file.file_exists("user://configfile"):
-#		DATA.gun_data = DATA.default_gun_data
-#		save_data_cfg()
-#		save_data_cfg()
-#		return false
-#	return true
+
 func new_game():
 	DATA.bullet_data = def_bullet_data.duplicate(true)
 	DATA.gun_data = def_gun_data.duplicate(true)
-
-#func save_data_cfg():
-#	DATA.config.save("user://configfile")
-#	DATA.config.set_value("guns","gun_data",DATA.gun_data)
-	
-	
-#func load_data_cfg():
-#	if start():	
-#		DATA.config.load("user://configfile")
-#		DATA.gun_data = config.get_value("guns","gun_data",DATA.gun_data)
